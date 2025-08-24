a4a906fe9d356d098a2bfee7c8ef6c1f8833b28a
Tofikbe <karategamingyt@gmail.com>
2025-08-24 20:08:30 +0530

    Update MemeToken.sol

--- contracts/contracts/MemeToken.sol
+++ contracts/contracts/MemeToken.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MemeToken is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address owner
+    ) ERC20(name, symbol) {
+        require(initialSupply <= 1_000_000_000, "Max supply 1B reached");
+        _mint(owner, initialSupply * 10 ** decimals());
+    }
+}
